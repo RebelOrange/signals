0a3f97da4e2d6bd324ef4cb95139bc1a7d76d449
dschmitt <david.schmitt@jt4llc.com>
2026-08-22 11:16:59 -0700

    added configuration menu

--- views/windows/init_view/init_view.py
+++ views/windows/init_view/init_view.py
@@ -8,7 +8,7 @@ from views.windows.run_window_as_app import *
 from PyQt5.QtWidgets import (
     QWidget, QVBoxLayout, QFormLayout, QHBoxLayout,
     QGroupBox, QCheckBox, QSpinBox, QLineEdit, QPushButton,
-    QMessageBox
+    QMessageBox, QComboBox
 )
 import re
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -50,24 +50,64 @@ class init_view(QWidget):
         return main_layout
 
     def _init_entry_layout(self, main_layout: QVBoxLayout):
-        # --- Group 1: General & Antenna Settings ---
-        grp_antenna = QGroupBox("Antenna & Target Setup")
+        # --- Group 0: General & Antenna Settings ---
+        grp_antenna = QGroupBox("Antenna Config")
         form_antenna = QFormLayout()
-
-        self.chk_tgt_on = QCheckBox()
-        self.chk_tgt_on.setChecked(True)  # Default: tgt_on=True
 
         self.spin_elements = QSpinBox()
         self.spin_elements.setRange(1, 128)
         self.spin_elements.setValue(8)  # Default: num_antenna_elements=8
 
+        self.sll_db = QLineEdit("25")
+        self.sll_db.setPlaceholderText("e.g. 25dB (negative)")
+
         self.txt_disconnected = QLineEdit("8")  # Default: disconnected_elements=[8]
         self.txt_disconnected.setPlaceholderText("e.g. 8 or 7, 8")
 
-        form_antenna.addRow("Target On:", self.chk_tgt_on)
-        form_antenna.addRow("Number of Elements:", self.spin_elements)
+        self.aux_gain_db= QLineEdit("0")  # Default: disconnected_elements=[8]
+        self.aux_gain_db.setPlaceholderText("e.g. 0dB")
+
+        self.main_pattern_weight = QComboBox()
+        self.main_pattern_weight.addItem("Conventional", userData="ula")
+        self.main_pattern_weight.addItem("Taylor", userData="taylor")
+        self.main_pattern_weight.addItem("Chebyshev", userData="cheby")
+
+        self.main_antenna_order = QSpinBox()
+        self.main_antenna_order.setRange(2, 128)
+        self.main_antenna_order.setValue(10)
+
+        form_antenna.addRow("Number of Aux Elements:", self.spin_elements)
         form_antenna.addRow("Disconnected Elements:", self.txt_disconnected)
+        form_antenna.addRow("Antenna Weighting:", self.main_pattern_weight)
+        form_antenna.addRow("Antenna Order:", self.main_antenna_order)
+        form_antenna.addRow("Sidelobe Level (dB):", self.sll_db)
+        form_antenna.addRow("Aux Channel Gain (dB):", self.aux_gain_db)
         grp_antenna.setLayout(form_antenna)
+
+        # --- Group 1: Target Signal Settings ---
+        grp_target = QGroupBox("Target Config")
+        form_target = QFormLayout()
+
+        self.chk_tgt_on = QCheckBox()
+        self.chk_tgt_on.setChecked(True)  # Default: tgt_on=True
+
+        self.tgt_sig_type = QComboBox()
+        self.tgt_sig_type.addItem("LFM", userData="lfm")
+        self.tgt_sig_type.addItem("Barker-13", userData="barker_13")
+        self.tgt_sig_type.addItem("CW", userData="cw")
+        self.tgt_sig_type.addItem("Noise", userData="noise")
+
+        self.tgt_gate = QLineEdit("0.4, 0.6")
+        self.tgt_gate.setPlaceholderText("Comma-separated Gate (Gate Fraction)")
+
+        self.tgt_timebandwidth_db = QLineEdit("10")
+        self.tgt_timebandwidth_db.setPlaceholderText("e.g. 10 (dB)")
+
+        form_target.addRow("Target On:", self.chk_tgt_on)
+        form_target.addRow("Signal Type:", self.tgt_sig_type)
+        form_target.addRow("Fractional Gate:", self.tgt_gate)
+        form_target.addRow("Time-Bandwidth Product (dB)", self.tgt_timebandwidth_db)
+        grp_target.setLayout(form_target)
 
         # --- Group 2: Jammer Signal Settings ---
         grp_jammers = QGroupBox("Jammer Configuration")
@@ -109,6 +149,7 @@ class init_view(QWidget):
         self.btn_preview.clicked.connect(self._on_preview_clicked)
 
         main_layout.addWidget(grp_antenna)
+        main_layout.addWidget(grp_target)
         main_layout.addWidget(grp_jammers)
         main_layout.addWidget(grp_gating)
         main_layout.addWidget(self.btn_preview)
@@ -143,6 +184,7 @@ class init_view(QWidget):
 
     def _update_antenna_preview(self, pattern_responses, az_points, pattern_labels, doas):
         ax = self.axes["spatial_plot"]
+        ax.clear()
         N, L = np.shape(pattern_responses) #N elements
         D = len(doas)
         print(f"number of responses: {N}")
@@ -168,15 +210,18 @@ class init_view(QWidget):
     def _on_preview_clicked(self):
         try:
             params = {
-                "tgt_on": self.chk_tgt_on.isChecked(),
                 "num_jammers": self.spin_num_jammers.value(),
                 "num_antenna_elements": self.spin_elements.value(),
+                "disconnected_elements": self._parse_ints(self.txt_disconnected.text()),
+                "main_order": self.main_antenna_order.value(),
+
+                "tgt_on": self.chk_tgt_on.isChecked(),
+
                 "jam_SNRs": self._parse_floats(self.txt_jam_snrs.text()),
                 "jam_bw_range": self._parse_floats(self.txt_bw_range.text()),
                 "doas": self._parse_floats(self.txt_doas.text()),
                 "min_gate_range_jammer": self._parse_floats(self.txt_min_gate.text()),
                 "max_gate_range_jammer": self._parse_floats(self.txt_max_gate.text()),
-                "disconnected_elements": self._parse_ints(self.txt_disconnected.text()),
             }
 
             # Emit parsed dictionary to Controller
@@ -228,9 +273,10 @@ if __name__ == "__main__":
 
         ssl_taylor = 25
         num_antenna_elements = params["num_antenna_elements"]
+        main_order = params["main_order"]
         # make antenna for spatial response plot
         antenna = ULA(num_elements=num_antenna_elements)
-        patterns = ([ElementPatterns.taylor_subarray(num_sub_elements=20, sll_db=ssl_taylor)]
+        patterns = ([ElementPatterns.taylor_subarray(num_sub_elements=main_order, sll_db=ssl_taylor)]
                     + [ElementPatterns.delta_subarray(4, gain_offset=(-ssl_taylor + 3))] * (num_antenna_elements - 1))
         antenna.set_disconnected_elements(params["disconnected_elements"])
         antenna.set_patterns(patterns)
